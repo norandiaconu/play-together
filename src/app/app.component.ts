f61a16ed43b8bb48af23acc464bb1f6568193561
Noran Diaconu <norandiaconu@gmail.com>
2024-12-20 22:38:15 -0500

    Added support for 2-4 players

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,14 +8,46 @@ import { Component } from '@angular/core';
 export class AppComponent {
     title = 'play-together';
     public playTogether: string[] = [];
+    public extraBoxes = 0;
+    public emptyBox = ["There's an empty box"];
 
     constructor() {}
 
-    click(box1: string, box2: string, box3: string): void {
+    increaseBoxes(): void {
+        this.extraBoxes++;
+    }
+
+    decreaseBoxes(): void {
+        this.extraBoxes--;
+    }
+
+    search(box1: string, box2: string, box3: string, box4: string): void {
+        if (box1 === '' || box2 === '') {
+            this.playTogether = this.emptyBox;
+            return;
+        }
         const list1 = this.getGames(box1);
         const list2 = this.getGames(box2);
-        const list3 = this.getGames(box3);
-        this.playTogether = list1.filter((item) => list2.includes(item) && list3.includes(item));
+        if (this.extraBoxes === 2) {
+            if (box3 === '' || box4 === '') {
+                this.playTogether = this.emptyBox;
+                return;
+            }
+            const list3 = this.getGames(box3);
+            const list4 = this.getGames(box4);
+            this.playTogether = list1.filter(
+                (item) => list2.includes(item) && list3.includes(item) && list4.includes(item)
+            );
+        } else if (this.extraBoxes === 1) {
+            if (box3 === '') {
+                this.playTogether = this.emptyBox;
+                return;
+            }
+            const list3 = this.getGames(box3);
+            this.playTogether = list1.filter((item) => list2.includes(item) && list3.includes(item));
+        } else {
+            this.playTogether = list1.filter((item) => list2.includes(item));
+        }
         setTimeout(() => {
             window.scrollTo({ top: document.body.scrollHeight, behavior: 'smooth' });
         }, 10);
